1998d2eddb71c1d6a49d829e95c5a90f671a3d3b
Daniel Havlik <dh@gocept.com>
2013-06-05 15:26:33 +0200

    re #39 feature for risclog.sqlalchemy to make json encoding of sqlalchemy mapped objects easier

---
+++ src/risclog/sqlalchemy/tests/test_model.py
@@ -0,0 +1,13 @@
+import unittest
+import mock
+
+
+class ObjectBaseTests(unittest.TestCase):
+
+    @mock.patch('risclog.sqlalchemy.serializer.sqlalchemy_encode')
+    def test__json__calls_custom_serializer(self, sql_enc):
+        import risclog.sqlalchemy.model
+        model = risclog.sqlalchemy.model.ObjectBase()
+        model.__json__({})
+        sql_enc.assert_called_once_with(model)
+
