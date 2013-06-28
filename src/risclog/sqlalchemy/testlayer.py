57c701b9cfe47e159b7a0baa71772e891861fbc4
Michael Howitz <mh@gocept.com>
2013-06-28 13:30:23 +0200

    Updated layer to smoothly support multiple database setups

--- src/risclog/sqlalchemy/testlayer.py
+++ src/risclog/sqlalchemy/testlayer.py
@@ -9,18 +9,21 @@ class DatabaseLayer(plone.testing.Layer):
 
     def __init__(self, name, factory, managed_tables=None):
         super(DatabaseLayer, self).__init__()
-        self._db_name = name
+        # name should be the engine name of the mapped classes
+        self.engine_name = name
+        self.name = 'database_' + name
         self._db_factory = factory
         self._db_managed_tables = managed_tables
 
     def setUp(self):
         plone.testing.zca.pushGlobalRegistry()
-        self[self._db_name] = risclog.sqlalchemy.testing.setUpDB(
-            self._db_factory)
+        self[self.name] = risclog.sqlalchemy.testing.setUpDB(
+            self._db_factory, name=self.engine_name)
 
     def tearDown(self):
-        risclog.sqlalchemy.testing.tearDownDB(self[self._db_name])
-        del self[self._db_name]
+        risclog.sqlalchemy.testing.tearDownDB(self[self.name],
+                                              name=self.engine_name)
+        del self[self.name]
         plone.testing.zca.popGlobalRegistry()
 
     def testSetUp(self):
