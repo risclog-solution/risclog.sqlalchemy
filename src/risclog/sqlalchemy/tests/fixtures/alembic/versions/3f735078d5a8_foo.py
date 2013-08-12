e94c0750a0da4958b46293916af3911f8cb349d7
Wolfgang Schnerring <ws@gocept.com>
2013-08-12 11:14:52 +0200

    Re #127: Implement support for alembic migrations (``assert_database_revision_is_current``)

---
+++ src/risclog/sqlalchemy/tests/fixtures/alembic/versions/3f735078d5a8_foo.py
@@ -0,0 +1,22 @@
+"""foo
+
+Revision ID: 3f735078d5a8
+Revises: None
+Create Date: 2013-08-12 10:59:01.394646
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3f735078d5a8'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
