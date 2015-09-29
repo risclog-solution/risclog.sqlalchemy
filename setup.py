a04943259f7da730ed4ffe497fd6c2f7cfc31bce
Michael Howitz <mh@gocept.com>
2015-09-29 17:31:58 +0200

    Add more classifiers.

--- setup.py
+++ setup.py
@@ -56,7 +56,13 @@ setup(
 
     keywords='sqlalchemy alembic model infrastructure utility',
     classifiers="""\
+Development Status :: 5 - Production/Stable
+Framework :: Pyramid
+Intended Audience :: Developers
+License :: OSI Approved
 License :: OSI Approved :: Zope Public License
+Natural Language :: English
+Operating System :: OS Independent
 Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
@@ -64,6 +70,11 @@ Programming Language :: Python :: 3
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
+Programming Language :: Python :: Implementation
+Programming Language :: Python :: Implementation :: CPython
+Topic :: Database
+Topic :: Software Development :: Libraries
+Topic :: Software Development :: Libraries :: Python Modules
 """[:-1].split('\n'),
     description=__doc__.strip(),
     namespace_packages=['risclog'],
