e90c0ffa92646c846b1ae98c03e3bed55ec866fa
Michael Howitz <mh@gocept.com>
2017-01-12 13:36:47 +0100

    Support Python 3.6.

--- setup.py
+++ setup.py
@@ -66,8 +66,10 @@ Programming Language :: Python :: 3
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Programming Language :: Python :: Implementation
 Programming Language :: Python :: Implementation :: CPython
+Programming Language :: Python :: Implementation :: PyPy
 Topic :: Database
 Topic :: Software Development :: Libraries
 Topic :: Software Development :: Libraries :: Python Modules
