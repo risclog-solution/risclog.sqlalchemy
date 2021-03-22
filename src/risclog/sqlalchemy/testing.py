b1176c9c8159d93d0d55ff62e5cff84afd97d6e8
Michael Howitz <mh@gocept.com>
2021-03-22 15:11:59 +0100

    WIP: Update to SQLAlchemy 1.4+

--- src/risclog/sqlalchemy/testing.py
+++ src/risclog/sqlalchemy/testing.py
@@ -34,8 +34,8 @@ def tearDownDB(db, name=''):
         db_util.drop_engine(name)
     # ...connections that have been checked-out from the pool and not yet
     # returned are not closed by dispose, either, so we have to hunt them
-    # down ourselves:
-    for conn in sqlalchemy.pool._refs:
+    # down ourselves (This is only necessary/possible for SQLAlchemy < 1.4.):
+    for conn in getattr(sqlalchemy.pool, '_refs', ()):
         conn.close()
     db.drop()
     if db_util and not db_util.get_all_engines():
